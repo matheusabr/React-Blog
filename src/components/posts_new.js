8f2779a13ea22ede435d1e553e7418848d6fa270
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-10 18:31:59 -0300

    chore(): add a link button from index to new post component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,6 +4,11 @@ class PostsNew extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-left">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a New Post
+          </Link>
+        </div>
         PostsNew
       </div>
     )
