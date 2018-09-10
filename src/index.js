964aafed6aa4b3b2b04cda10121acb019c054f3a
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-10 10:44:51 -0300

    chore(npm): add react-router-dom@4.2.2 and test a simple route history

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hi extends React.Component {
+  render() {
+    return <div>Hi!!!</div>
+  }
+}
+
+class Bye extends React.Component {
+  render() {
+    return <div>Bye!!!</div>
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <h2>Header</h2>
+        <Route path="/hi" component={Hi} />
+        <Route path="/bye" component={Bye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
